1e8a7797df4f5d800c538370c41397ad30766d0f
subodhrana390 <subodhrana390@gmail.com>
2024-04-13 12:49:58 +0530

    fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,25 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { useQuery, gql } from "@apollo/client";
+import Loader from "./Loader";
 
 const Banner = () => {
+  const query = gql`
+    query MyQuery {
+      banners {
+        title
+        image {
+          url
+        }
+      }
+    }
+  `;
+
+  const { data, loading, error } = useQuery(query);
+
+  if (loading) return <Loader />;
+  if (error) return <pre>{error.message}</pre>;
   return (
     <Swiper
       spaceBetween={30}
@@ -21,20 +38,20 @@ const Banner = () => {
       modules={[Autoplay, Pagination, Navigation]}
       className="w-full h-full"
     >
-      <SwiperSlide className="text-center font-[18px] bg-white flex justify-center items-center h-full">
-        <img
-          src="https://images.unsplash.com/photo-1613418337534-aa921419f75e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          className="h-[500px] w-full object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide className="text-center font-[18px] bg-white flex justify-center items-center h-full">
-        <img
-          src="https://images.unsplash.com/photo-1712303700832-57d2b2b916b8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          className="h-[500px] w-full object-center md:object-cover"
-        />
-      </SwiperSlide>
+      {data.banners.length > 0 ? (
+        <>
+          {data.banners.map((ban) => (
+            <SwiperSlide className="text-center font-[18px] bg-white flex justify-center items-center h-full">
+              <img
+                src={ban.image.url}
+                className="h-[500px] w-full object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </>
+      ) : (
+        ""
+      )}
     </Swiper>
   );
 };
